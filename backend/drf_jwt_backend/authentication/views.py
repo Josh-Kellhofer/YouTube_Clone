f92a82f44f30d66ed9c7b344d0e5893c4c181956
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-03-17 09:58:53 -0700

    Added imports.

--- backend/drf_jwt_backend/authentication/views.py
+++ backend/drf_jwt_backend/authentication/views.py
@@ -3,6 +3,13 @@ from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CommentSerializer
+from .serializers import ReplySerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import status
+from rest_framework import Response
+
 User = get_user_model()
 
 
@@ -14,3 +21,4 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
