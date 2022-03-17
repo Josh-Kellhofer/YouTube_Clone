da830a5313463b4f45b370f3378d6dd0333cf80d
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-03-17 15:21:57 -0700

    Added Admin Code for site.

--- backend/drf_jwt_backend/comments/urls.py
+++ backend/drf_jwt_backend/comments/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from comments import views
 
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
-    path('', views.user_comments),
+    # path('', views.user_comments),
     path('all/', views.get_all_comments),
 ]
