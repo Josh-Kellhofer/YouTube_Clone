43593f09fb699dc772e851d2f7b89660b4ab9f8c
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-03-17 11:03:46 -0700

    Created GET for All Comments.

--- backend/drf_jwt_backend/authentication/views.py
+++ backend/drf_jwt_backend/authentication/views.py
@@ -9,6 +9,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from rest_framework import Response
+from .models import Comment
 
 User = get_user_model()
 
@@ -22,3 +23,9 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
 
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
