dcfe4de9e495bbbddc185b3cc43b8642f596083f
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-03-23 08:10:04 -0700

    Pull after push Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import AddCarPage from "./pages/AddCarPage/AddCarPage";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -15,10 +16,31 @@ import VideoPlayer from "./components/VideoPlayer/videoplayer";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
+import axios from "axios";
+import React, { useState, useEffect} from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
+
+
 
 function App() {
+
+  const [searchResults, setSearchResults] = useState([""]);
+
+  useEffect(() => {
+    getSearchResults()
+  }, [])
+
+  async function getSearchResults(searchTerm="Soweto Gospel Choir Khumbya"){
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&key={API KEY HERE}`);
+    console.log(response.data)
+    setSearchResults(response.data)
+  }
+
   return (
-    <div>
+    <div className="App">
+      <div className="Search-Bar">
+        <SearchBar getSearchResults={getSearchResults}/>
+      </div>
       <Navbar />
        <Routes>
         <Route
